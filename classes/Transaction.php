e3075769b59b91d358536189504d2391948b9d04
Lasith-Niro <lasithniro.online@gmail.com>
2015-08-14 22:20:55 +0530

    new

--- classes/Transaction.php
+++ classes/Transaction.php
@@ -17,13 +17,13 @@ class transaction{
         $decrpt = new decrypt();
         return $decrpt;
     }
-    public function send($url, $data){
-        $sendingString = http_build_query($data);
-        $channel = curl_init($url);
-        curl_setopt($channel, CURLOPT_POST, true);
-        curl_setopt($channel, CURLOPT_POSTFIELDS, $sendingString);
-        curl_setopt($channel, CURLOPT_RETURNTRANSFER, true);
-        curl_exec($channel);
-        curl_close($channel);
-    }
+//    public function send($url, $data){
+//        $sendingString = http_build_query($data);
+//        $channel = curl_init($url);
+//        curl_setopt($channel, CURLOPT_POST, true);
+//        curl_setopt($channel, CURLOPT_POSTFIELDS, $sendingString);
+//        curl_setopt($channel, CURLOPT_RETURNTRANSFER, true);
+//        curl_exec($channel);
+//        curl_close($channel);
+//    }
 }
