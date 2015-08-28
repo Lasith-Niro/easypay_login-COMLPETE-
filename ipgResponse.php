a4d9ce99da22a832ad01cac6647257ee7c03c5af
Lasith-Niro <lasithniro.online@gmail.com>
2015-08-28 10:03:13 +0530

    add new fetures

--- ipgResponse.php
+++ ipgResponse.php
@@ -14,30 +14,30 @@ $user = new User();
 $dec = new decrypt();
 $transaction = new Transaction();
 
-//$encrypted = $_POST['merchantReciept'];
-//$decryptObject = $dec->decode($encrypted);
-//$decArray = explode('|',$decryptObject);
+$encrypted = $_POST['merchantReciept'];
+$decryptObject = $dec->decode($encrypted);
+$decArray = explode('|',$decryptObject);
 
 if(!$user->isLoggedIn()){
     Redirect::to('index.php');
 }
 
 
-/*
 $transactionID     = $decArray[0];
 $statusCode        = $decArray[1];
 $statusDescription = $decArray[2];
 $transactionAmount = $decArray[3];
 $merchantCode      = $decArray[4];
 $walletReferenceID = $decArray[5];
-*/
 
+/* TEST DATA
 $transactionID     = '001';
 $statusCode        = 2;
 $statusDescription = "test payment";
 $transactionAmount = 10.00;
 $merchantCode      = "TESTMERCHANT";
 $walletReferenceID = '5001';
+*/
 
 $userId = $user->data()->id;
 $curDate = date("Y-m-d");
@@ -57,53 +57,55 @@ if(Input::exists()){
         switch($statusCode){
             case 2: //Completed transaction
                 //Type success code here
-                echo "transaction success";
+                $str = "transaction success";
                 break;
             case 3: //Failed
-                echo "Transaction failed";
+                $str = "Transaction failed";
                 break;
             case 4: //System error
-                echo "System error";
+                $str = "System error";
                 break;
             case 5: //Invalid customer
-                echo "tho horek";
+                $str = "tho horek";
                 break;
             case 6: //invalid customer status
-                echo "Customer status invalid";
+                $str = "Customer status invalid";
                 break;
             case 7: //customer account lock
-                echo "Your ezcash account is locked";
+                $str = "Your ezcash account is locked";
                 break;
             case 8: //Invalid transaction type
-                echo "Transaction type invalid";
+                $str = "Transaction type invalid";
                 break;
             case 9: //Unothorized transaction type
-                echo "Transaction type unothorized";
+                $str = "Transaction type unothorized";
                 break;
             case 10: //Invalid agent
-                echo "Agent invalid";
+                $str = "Agent invalid";
                 break;
             case 11: //Invalid agent status
-                echo "Agent status invalid";
+                $str = "Agent status invalid";
                 break;
             case 12: //Entered amount is not in between max or min limits
-                echo "Entered amount is not in between max or min limits";
+                $str = "Entered amount is not in between max or min limits";
                 break;
             case 13: //eMoney transaction failure
-                echo "eMoney transaction failed";
+                $str = "eMoney transaction failed";
                 break;
             case 14: //Transaction committing failure
-                echo "Failed transaction committing";
+                $str = "Failed transaction committing";
                 break;
             case 15: //Customer account blocked due to invalid PIN retries
-                echo "Your account is blocked due to invalid PIN retries";
+                $str = "Your account is blocked due to invalid PIN retries";
                 break;
             case 16: //Active session expired
-                echo "Active session expired";
+                $str = "Active session expired";
                 break;
         //    default:
         //        echo "Transaction failed";
         }
+        Session::flash('home', $str);
+        Redirect::to('index.php');
     }
 }
 
