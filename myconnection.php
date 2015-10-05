2253e1173aa34b5a2b5548025aa80cadc56ba5ff
lahiru <lahirupathiranalr@gmail.com>
2015-10-05 20:36:45 +0530

    tempory connection for dash board working..

---
+++ myconnection.php
@@ -0,0 +1,9 @@
+<?php
+$mysql_host = "localhost";
+$mysql_user = "root";
+$mysql_pass = "";
+$mysl_database = "easypay_db";
+
+$conn = mysqli_connect($mysql_host, $mysql_user, $mysql_pass,$mysl_database);
+//mysql_select_db($mysl_database, $conn);
+?>
