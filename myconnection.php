59f42f25b56cc7f43dec05a3a0f7cd3ff6937df9
lahiru <lahirupathiranalr@gmail.com>
2015-10-11 13:15:16 +0530

    deleted myConnection.php

--- myconnection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysql_host = "localhost";
-$mysql_user = "root";
-$mysql_pass = "";
-$mysl_database = "easypay_db";
-
-$conn = mysqli_connect($mysql_host, $mysql_user, $mysql_pass,$mysl_database);
-//mysql_select_db($mysl_database, $conn);
-?>
