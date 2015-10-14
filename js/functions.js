a196ced62b7c380029fec24da5ea9d13ca9dbd34
lahiru <lahirupathiranalr@gmail.com>
2015-10-14 09:01:39 +0530

    add and remove multiple subject forms functions.

--- js/functions.js
+++ js/functions.js
@@ -1,34 +1,34 @@
 /**
  * Created by lahiru on 9/20/2015.
  */
-
-////////////////////// add username input field ///////////////////////
-var counterText = 0;
-var counterLimit = 1;
-function addUsernameTextField(divName){
-    var field = document.createElement('div');
-    if (counterText != counterLimit) {
-        field.innerHTML = "<input type='text' name='username' placeholder='Username'>";
-        counterText++;
+var flimit = 3;
+var counter = 0;
+function createCopy(){
+    if(counter<flimit-1){
+        counter++;
+        var itm = document.getElementById("subjectDet");
+        var cln = itm.cloneNode(true);
+        document.getElementById("container").appendChild(cln);
     }
-    document.getElementById(divName).appendChild(field);
-    field.disabled();
+    if(counter==flimit-1){
+        alert('You have reached the maximum limit of '+flimit+' forms.');
+    }
 }
 
-//////////////////////////////////// end of function ///////////////////////////////////////
 
+///////////
 
-//// This is the event handler to check the registered OTHER PERSON s/////
-$(document).ready(function(){
-    $('#cb1').click(function(){
-        if($(this).prop("checked") == true){
-            alert("Checked");
-            $('#cb1_feedback').html('Get username only');
-        }
-        else if($(this).prop("checked") == false){
-            alert("Unchecked");
-            $('#cb1_feedback').html('Get all details');
-        }
-    });
-});
-/////// END ///////
+function removeCopy(){
+    if(counter>0){
+        document.getElementById("subjectDet").remove();
+        counter--;
+    }
+    if(counter==0){
+        alert('You have reached the minimum number of forms');
+    }
+}
+
+function addSubjectCount(){
+    var $newDiv = $("<div><h3>Subject Details number "+counter+" </h3></div>");
+    $newDiv.appendTo($("#container"));
+}
